e49f6bd0504402b3810c4a7a5d72170423539621
niklas-hjelm <niklas.hjelm@iths.se>
2024-05-22 15:37:05 +0200

    update

--- src/Frontend/OmegaLulChat/Program.cs
+++ src/Frontend/OmegaLulChat/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddHttpClient("chatApi", o=>  o.BaseAddress = new Uri("https://localhost:7194"));
 
 await builder.Build().RunAsync();
