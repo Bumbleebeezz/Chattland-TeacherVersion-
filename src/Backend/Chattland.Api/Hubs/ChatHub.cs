1181b525f59fc70975ab16a96a274e473dde730d
niklas-hjelm <niklas.hjelm@iths.se>
2024-05-27 11:39:17 +0200

    update

---
+++ src/Backend/Chattland.Api/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Chattland.Api.DataAccess;
+using Chattland.Api.DataAccess.Entities;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chattland.Api.Hubs;
+
+public class ChatHub(IChatMessageRepository repo) : Hub
+{
+    public async Task SendMessage(ChatMessage message)
+    {
+        await repo.AddOneAsync(message);
+        await Clients.All.SendAsync("SendMessage", message);
+    }
+}
--- src/Backend/Chattland.Api/Program.cs
+++ src/Backend/Chattland.Api/Program.cs
@@ -1,13 +1,32 @@
 using Chattland.Api.DataAccess;
 using Chattland.Api.DataAccess.Entities;
+using Chattland.Api.Hubs;
 using Chattland.DataTransferContract.ChatContracts;
 using Microsoft.AspNetCore.Components.Endpoints;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataAccess();
 
+builder.Services
+	.AddCors(options =>
+			options.AddPolicy(
+				"OmegaLulChat",
+				policy =>
+					policy
+						.WithOrigins("https://localhost:7194", "https://localhost:7053")
+						.AllowAnyMethod()
+						.AllowAnyHeader()
+						.AllowCredentials()
+				)
+		);
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+
+app.UseCors("OmegaLulChat");
 
 app.MapPost("/messages/{room}", async (IChatMessageRepository repo, ChatMessage message, string room) =>
 {
@@ -26,5 +45,7 @@ app.MapGet("/messages/rooms", async (IChatMessageRepository repo) =>
 {
 	return await repo.GetRoomNames();
 });
+
+app.MapHub<ChatHub>("/hubs/Chat");
 
 app.Run();
